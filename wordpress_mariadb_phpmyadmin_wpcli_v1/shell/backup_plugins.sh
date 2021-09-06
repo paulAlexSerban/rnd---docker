1a8d1c516931f38a4a0dcd12a5335045de3c1903
Paul Serban <paul.alex.serban@gmail.com>
2021-09-06 10:24:17 +0300

    docker_17 | updated | build & deploy config

--- wordpress_mariadb_phpmyadmin_wpcli_v1/shell/backup_plugins.sh
+++ wordpress_mariadb_phpmyadmin_wpcli_v1/shell/backup_plugins.sh
@@ -3,4 +3,4 @@
 export $(grep -v '^#.*' .env | xargs)
 
 echo 'Copy installed plugins to ./src'
-cp -r ./wordpress/wp-content/plugins ./src
+cp -r ./wordpress/wp-content/themes/test-wordpress-project/plugins/* ./src
--- wordpress_mariadb_phpmyadmin_wpcli_v1/shell/backup_uploads.sh
+++ wordpress_mariadb_phpmyadmin_wpcli_v1/shell/backup_uploads.sh
@@ -3,4 +3,4 @@
 export $(grep -v '^#.*' .env | xargs)
 
 echo 'Copy uploads to ./src'
-cp -r ./wordpress/wp-content/uploads ./src
+cp -r ./wordpress/wp-content/themes/test-wordpress-project/uploads/* ./src
